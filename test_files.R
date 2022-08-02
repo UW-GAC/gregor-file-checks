13ba11a85fe8b080b0714ddd1e62fbfc2e52361f
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-08-02 14:52:28 -0700

    update test files for compatibility with data model

--- test_files.R
+++ test_files.R
@@ -9,7 +9,7 @@ participant <- tibble(
     family_id = paste0("UW_DCC_FAM", c(rep("001", 3), "002")),
     paternal_id = c("UW_DCC_participant_124", rep(0, 3)),
     maternal_id = c("UW_DCC_participant_125", rep(0, 3)),
-    proband_relationship = c("self", "mother", "father", "other"),
+    proband_relationship = c("Self", "Mother", "Father", "Other"),
     proband_relationship_detail = c(rep("", 3), "Third cousins, through mother's side"),
     sex = c("Male", "Female", "Male", "Female"),
     sex_detail = c("XXY expected", "", "", "XY expected"),
@@ -17,7 +17,7 @@ participant <- tibble(
     reported_ethnicity = c(rep("Not Hispanic or Latino", 3), "Unknown"),
     ancestry_detail = c("", "", "Polish", ""),
     age_at_last_observation = c(6.5, 35, 37, 28),
-    affected_status = c("Affected", "Unknown", rep("Possibly Affected", 2)),
+    affected_status = c("Affected", "Unknown", rep("Possibly affected", 2)),
     phenotype_description = c("Distal arthrogryposis with stellate teeth", "mother of participant_123", "father of participant_123", "Missing one variant in gene XXX"),
     age_at_enrollment = c(4, 33, 35, 28)
 )
@@ -30,7 +30,7 @@ family <- tibble(
 phenotype <- tibble(
     participant_id = paste0("UW_DCC_participant_", c(123, 125, 125)),
     term_id	= c("HP:0001627", "HP:0001637", "HP:0001647"),
-    presence = c("present", "absent", "present"),
+    presence = c("Present", "Absent", "Present"),
     ontology = "HPO",
     additional_details = c("", "cardiac abnormality differs from participant_123", ""),
     onset_age_range	= c("", "", "HP:0003577"),
@@ -51,8 +51,7 @@ experiment_dna_short_read <- tibble(
     seq_library_prep_kit_method = "Kappa Hyper PCR plus",
     read_length = 100,
     experiment_type = "exome",
-    targeted_region_bed_filename = "experiment_1.bed",
-    targeted_region_bed_file_uri = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bed",
+    targeted_region_bed_file = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bed",
     date_data_generation = "2022-06-29",
     target_insert_size = 600,
     sequencing_platform = "Illumina NovaSeq6000"
@@ -62,7 +61,7 @@ aligned_dna_short_read = tibble(
     aligned_dna_short_read_id = "UW_DCC_H7YG5DSX2-3-IDUDI0014-1",
     experiment_dna_short_read_id = "UW_DCC_experiment_1",
     aligned_dna_short_read_file = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bam",
-    aligned_short_read_dna_index_file = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bai",
+    aligned_dna_short_read_index_file = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bai",
     md5sum = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/experiment_1.bai",
     reference_assembly = "GRCh38",
     alignment_software = "BWA-MEM-2.3",
