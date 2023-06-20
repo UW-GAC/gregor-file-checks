aca4487ccaf689ab741bf3adddccc8546c2fb8c6
Stephanie M. Gogarten <sdmorris@uw.edu>
2023-06-20 11:33:14 -0700

    update examples

--- data_model_examples/test_files.R
+++ data_model_examples/test_files.R
@@ -3,7 +3,7 @@ participant <- tibble(
     participant_id = paste0("UW_DCC_participant_", 123:126),
     gregor_center = "UW_DCC",
     consent_code = c(rep("GRU", 2), rep("HMB", 2)),
-    recontactable = c("Yes", "Yes", "No", "Unknown"),
+    recontactable = c("Yes", "Yes", "No", "No"),
     prior_testing = c("Normal karyotype | FBN2 screened clinically", "", "Abnormal karyotype", ""),
     pmid_id = c("25683120", "", "25683120|25683125", ""),
     family_id = paste0("UW_DCC_FAM", c(rep("001", 3), "002")),
@@ -14,7 +14,7 @@ participant <- tibble(
     sex = c("Male", "Female", "Male", "Female"),
     sex_detail = c("XXY expected", "", "", "XY expected"),
     reported_race = c(rep("American Indian or Alaska Native", 2), "White", "Black or African American"),
-    reported_ethnicity = c(rep("Not Hispanic or Latino", 3), "Unknown"),
+    reported_ethnicity = c(rep("Not Hispanic or Latino", 3), NA),
     ancestry_detail = c("", "", "Polish", ""),
     age_at_last_observation = c(6.5, 35, 37, 28),
     affected_status = c("Affected", "Unknown", rep("Possibly affected", 2)),
@@ -79,13 +79,41 @@ called_variants_dna_short_read <- tibble(
     called_variants_dna_file = "gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/variants_file.vcf",
     md5sum = "efdf612a01049cc6e99470ad92b0326e",
     caller_software = "gatk4.1.2",
-    variant_types = "snv|indel",
+    variant_types = "SNV|INDEL",
     analysis_details = "10.5281/zenodo.4469317"
 )
 
+experiment_rna_short_read <- tibble(
+    experiment_rna_short_read_id = c("UW_DCC_experiment_1", "UW_DCC_experiment_2"),
+    analyte_id = c("UW_DCC_analyte_1", "UW_DCC_analyte_2"),
+    experiment_sample_id = c("H7YG5DSX2-3-IDUDI0014-1", "H7YG5DSX2-5-IDUDI0126-1"),
+    seq_library_prep_kit_method = "Kappa Hyper PCR plus",
+    read_length = 100,
+    experiment_type = "paired-end | targeted",
+    single_or_paired_ends = "paired-end",
+    date_data_generation = "2022-06-29",
+    sequencing_platform = "Illumina NovaSeq6000",
+    within_site_batch_name = "Batch 4",
+    RIN = 9.8
+)
+
+aligned_rna_short_read = tibble(
+    aligned_rna_short_read_id = c("UW_DCC_H7YG5DSX2-3-IDUDI0014-1", "UW_DCC_H7YG5DSX2-5-IDUDI0126-1"),
+    experiment_rna_short_read_id = c("UW_DCC_experiment_1", "UW_DCC_experiment_2"),
+    aligned_rna_short_read_file = paste0("gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/rna_experiment_", 1:2, ".bam"),
+    aligned_rna_short_read_index_file = paste0("gs://fc-eb352699-d849-483f-aefe-9d35ce2b21ac/rna_experiment_", 1:2, ".bai"),
+    md5sum = c("a3b07384d113edec49eaa6238ad5ff00", "b7457a3403aa8e4616ce64bfae86655c"),
+    reference_assembly = "GRCh38",
+    alignment_software = "BWA-MEM-2.3",
+    mean_coverage = 100,
+    gene_annotation = "GENCODEv35",
+    gene_annotation_details = "gencode_comprehensive_chr"
+)
+
 table_names <- c("participant", "family", "phenotype", "analyte", "experiment_dna_short_read",
-    "aligned_dna_short_read", "aligned_dna_short_read_set", "called_variants_dna_short_read")
+    "aligned_dna_short_read", "aligned_dna_short_read_set", "called_variants_dna_short_read",
+    "experiment_rna_short_read", "aligned_rna_short_read")
 for (t in table_names) {
-    outfile <- paste0("testdata/", t, ".tsv")
+    outfile <- paste0(t, ".tsv")
     readr::write_tsv(get(t), outfile)
 }
